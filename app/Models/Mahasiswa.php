120f3ea04ae3680cc7dfc8381f06d4f23fef1488
EdoLaksana <edolaksana02@gmail.com>
2022-09-20 11:01:50 +0700

    editdatamhs

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -10,6 +10,7 @@ class Mahasiswa extends Model
     protected $primaryKey       = 'id';
     protected $useAutoIncrement = true;
     protected $useTimetamps     = true;
+    protected $allowedFields      = ['npm', 'nama', 'alamat'];
     protected $dateformat       = 'datetime';
     protected $createdFields    = 'created_at';
     protected $updatedFields    = 'updated_at';
--- app/Views/mahasiswa/list.php
+++ app/Views/mahasiswa/list.php
@@ -7,7 +7,8 @@
                 <th scope="col">NPM</th>
                 <th scope="col">Nama</th>
                 <th scope="col">Alamat</th>
-                <th scope="col">Created_At</th>
+                <th scope="col">Creater_At</th>
+                <th scope="col">Action</th>
             </tr>
         </thead>
         <tbody>
@@ -18,7 +19,16 @@
                     <td><?= $mhs['npm'] ?></td>
                     <td><?= $mhs['nama'] ?></td>
                     <td><?= $mhs['alamat'] ?></td>
-                    <td><?= $mhs['created_at'] ?></td>
+                    <td><?= $mhs['creater_at'] ?></td>
+                    <td>
+                        <div class="d-flex">
+                            <a class="btn btn-warning mr-3" href=/edit/<?= $mhs['id'] ?>">Edit</a>
+                        </div>
+                        <form action="/delete/<?= $mhs['id'] ?>" method="post">
+                            <input type="hidden" name="_method" value="DELETE">
+                            <button type="submit" class="btn btn-danger">Delete
+                        </form>
+                    </td>
                 </tr>
             <?php $no++;
             endforeach; ?>
